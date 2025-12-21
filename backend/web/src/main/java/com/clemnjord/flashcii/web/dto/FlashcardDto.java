4df4be39c5d7aa3d855b13768ab269fbf6e7c92c
clemnjord <clementpuech@protonmail.com>
2025-12-21 22:17:02 +0100

    30 - Take into account PR review

--- backend/web/src/main/java/com/clemnjord/flashcii/web/dto/FlashcardDto.java
+++ backend/web/src/main/java/com/clemnjord/flashcii/web/dto/FlashcardDto.java
@@ -5,7 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 public class FlashcardDto {
     public record FlashcardCreateRequest(
             @NotBlank(message = "Question is required") String question,
-            @NotBlank(message = "Answer is required") String answer) {}
+            @NotBlank(message = "Answer is required") String answer) {
+    }
 
     public record FlashcardResponse(String uuid, String question, String answer) {}
 }
--- backend/web/src/main/java/com/clemnjord/flashcii/web/dto/QuizDto.java
+++ backend/web/src/main/java/com/clemnjord/flashcii/web/dto/QuizDto.java
@@ -1,15 +1,29 @@
 package com.clemnjord.flashcii.web.dto;
 
+import io.github.openspacedrepetition.Rating;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
+import java.util.UUID;
 
 public class QuizDto {
     public record QuizResponse(
-            @NotNull String uuid, @NotEmpty List<String> flashcardIds) {}
+            String uuid, List<String> flashcardIds) {}
 
-    public record CreateQuizRequest(@NotEmpty List<String> deckIds) {}
+    public record CreateQuizRequest(@NotNull @NotEmpty List<String> deckIds) {
+    }
 
     public record RateFlashcardRequest(
-            @NotNull String flashcardUuid, @NotNull String rating) {}
+            @NotBlank(message = "Flashcard UUID can't be blank") String flashcardUuid,
+            @NotNull(message = "Rating can't be blank") String rating) {
+
+        public RateFlashcardRequest {
+            // Validate that flashcardUuid is a valid UUID
+            UUID.fromString(flashcardUuid);
+
+            // Validate that rating is one of the allowed values
+            Rating.valueOf(rating);
+        }
+    }
 }
