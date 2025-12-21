b58005fd7da4835e048c2aa1e772f950b893e7e7
clemnjord <clementpuech@protonmail.com>
2025-12-21 22:21:36 +0100

    30 - Take into account PR review

--- backend/web/src/main/java/com/clemnjord/flashcii/web/dto/QuizDto.java
+++ backend/web/src/main/java/com/clemnjord/flashcii/web/dto/QuizDto.java
@@ -1,29 +1,17 @@
 package com.clemnjord.flashcii.web.dto;
 
-import io.github.openspacedrepetition.Rating;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
-import java.util.UUID;
 
 public class QuizDto {
-    public record QuizResponse(
-            String uuid, List<String> flashcardIds) {}
+    public record QuizResponse(String uuid, List<String> flashcardIds) {}
 
-    public record CreateQuizRequest(@NotNull @NotEmpty List<String> deckIds) {
-    }
+    public record CreateQuizRequest(@NotNull @NotEmpty List<String> deckIds) {}
 
     public record RateFlashcardRequest(
             @NotBlank(message = "Flashcard UUID can't be blank") String flashcardUuid,
-            @NotNull(message = "Rating can't be blank") String rating) {
 
-        public RateFlashcardRequest {
-            // Validate that flashcardUuid is a valid UUID
-            UUID.fromString(flashcardUuid);
-
-            // Validate that rating is one of the allowed values
-            Rating.valueOf(rating);
-        }
-    }
+            @NotNull(message = "Rating can't be blank") String rating) {}
 }
